e5557fc29506753b59641ece89a43b4795bb806e
Daniel Ruiz García <daniel.ruiz@wizeline.com>
2020-09-27 23:14:53 -0500

    made login less ugly

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
-
 import { useAuth } from '../../providers/Auth';
+import styled from 'styled-components';
 import './Login.styles.css';
+
+const LoginStyle = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -27,24 +33,26 @@ function LoginPage() {
   };
 
   return (
-    <section className="login">
-      <h1>Welcome back!</h1>
-      <form onSubmit={myAuthenticate} className="login-form">
-        <div className="form-group">
-          <label htmlFor="username">
-            <strong>username </strong>
-            <input required type="text" id="username" onChange={getUser} />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">
-            <strong>password </strong>
-            <input required type="password" id="password" onChange={getPassword} />
-          </label>
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </section>
+    <LoginStyle>
+      <section className="login">
+        <h1>Welcome back!</h1>
+        <form onSubmit={myAuthenticate} className="login-form">
+          <div className="form-group">
+            <label htmlFor="username">
+              <strong>username </strong>
+              <input required type="text" id="username" onChange={getUser} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">
+              <strong>password </strong>
+              <input required type="password" id="password" onChange={getPassword} />
+            </label>
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </section>
+    </LoginStyle>
   );
 }
 
